5f70951c00d65e50970d105d17fa89be48a09025
annika-miller <amiller42@smith.edu>
2019-12-08 11:35:51 -0500

    comments, readme

--- src/main/java/YearlyMembership.java
+++ src/main/java/YearlyMembership.java
@@ -3,12 +3,17 @@ import java.time.Period;
 
 public class YearlyMembership extends Membership{
 
+    //rides that come included in membership each year
     private final int maxRides = 260;
 
     public YearlyMembership() {
         super();
     }
 
+    /**
+     * checks whether it is time to renew the monthly membership
+     * @return true if it has been 365+ days since last payment, false if less
+     */
     public boolean checkPaymentDue(){
         LocalDate now = LocalDate.now();
         Period period = Period.between(now, super.getLastPayment());
@@ -16,14 +21,17 @@ public class YearlyMembership extends Membership{
         return (diff >= 365);
     }
 
+    //checks whether there are 0 free rides left
     public boolean checkMaxRidesExceeded(){
         return super.getTotalRidesLeft() >= maxRides;
     }
 
+    //returns 3, which represents a yearly membership
     public int getMembershipInt(){
         return 3;
     }
 
+    //returns type of membership as a string
     public String getMembershipString(){
         return "Yearly";
     }
